9417218e67d7e30a8c0b5453247cd76d4988c073
VajjaPraveen <147692584+VajjaPraveen@users.noreply.github.com>
2024-01-19 11:53:47 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import { Component } from "react";
+import NxtSideBar from "../NxtSideBar";
+import NxtHeader from "../NxtHeader";
+import NxtPremium from "../NxtPremium";
+import NxtHomeVideos from "../NxtHomeVideos";
+import NxtWrapper from "../NxtWrapper";
+class NxtHome extends Component {
+    state={queryParameter:'',initialUserValue:''}
+   
+    changeInitialValue=(newValue)=>{
+        this.setState({initialUserValue:newValue})
+    }
+    
+    render() {
+        
+        return (<>
+            {/* <NxtHeader /> */}
+            <NxtWrapper/>
+            <div className="NxtHomeContainer">
+                <NxtSideBar />
+                <div>
+                    <NxtPremium />
+                    <NxtHomeVideos />
+                </div>
+            </div>
+        </>) 
+    }
+}
+export default NxtHome
