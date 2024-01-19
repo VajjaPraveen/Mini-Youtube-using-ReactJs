ba0f0c05d206c6586c37e2b219fc957eb4fa0963
VajjaPraveen <147692584+VajjaPraveen@users.noreply.github.com>
2024-01-19 18:11:48 +0530

    Premium.js

---
+++ src/Premium.js
@@ -0,0 +1,12 @@
+const NxtPremium = () => {
+    return (<div className="NxtPremiumContainer">
+        <div className="NxtPremiumContainerItem1">
+            <img src="https://assets.ccbp.in/frontend/react-js/nxt-watch-logo-light-theme-img.png" alt="webSiteLogo" className="NxtPremiumLogo"></img>
+            <h1>Buy Next Premium prepaid plans with UPI</h1>
+        </div>
+        <button>Get Now</button>
+
+
+    </div>)
+}
+export default NxtPremium
